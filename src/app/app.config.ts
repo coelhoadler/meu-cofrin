f1f7c89efc66177c4096cf0b349a5e29a4db7e9f
Adler Santos <adler.coelho.santos@icloud.com>
2026-05-28 20:52:39 -0300

    feat: initialize Angular project structure and configure Firebase hosting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,45 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
+
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+registerLocaleData(ptBr);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideZonelessChangeDetection(),
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'pt' },
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: "AIzaSyAPDHqVLWlphds13yTj_YK_P2irI_lVLG0",
+        authDomain: "meu-cofrin.firebaseapp.com",
+        projectId: "meu-cofrin",
+        storageBucket: "meu-cofrin.firebasestorage.app",
+        messagingSenderId: "570567455946",
+        appId: "1:570567455946:web:ea39f614bd1397991f7033",
+        measurementId: "G-QHZ9Q0MRZS"
+      }),
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideEnvironmentNgxMask(), provideClientHydration(withEventReplay()),
+  ],
 };
+
