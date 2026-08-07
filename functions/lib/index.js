e6c83c8b9b2fe7356dd6fe1dcfeb367e51d10dd3
Adler Santos <adler.coelho.santos@icloud.com>
2026-08-06 21:30:13 -0300

    feat: implement scheduled re-engagement email for inactive users and add test endpoint

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -19,6 +19,8 @@ admin.initializeApp();
 __exportStar(require("./scheduled/notificarContasVencendo"), exports);
 __exportStar(require("./scheduled/notificarContasPorEmail"), exports);
 __exportStar(require("./scheduled/consolidarResumosMensais"), exports);
+__exportStar(require("./scheduled/notificarUsuariosInativos"), exports);
+__exportStar(require("./testNotificarUsuariosInativos"), exports);
 __exportStar(require("./auth/webauthn"), exports);
 __exportStar(require("./triggers/onContaWritten"), exports);
 //# sourceMappingURL=index.js.map
